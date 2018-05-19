dd47ff94ec57551b1c20d66e21c01463206a2043
TrentHether <38933187+TrentHether@users.noreply.github.com>
2018-05-20 11:00:29 +1200

    Added what I know
    Remove comments if you find them to be an issue

--- Source/networkGate.cpp
+++ Source/networkGate.cpp
@@ -1,16 +1,18 @@
+ //
 #include <stdio.h>
 #include "E101.h"
 #include "networkGate.h"
 
 void openGate() {
 	int i = -1;
+	char server[] = "130.195.6.196";//Server IP may change
 	
 	while(i < 0){
-		i = connect_to_server("130.195.6.196", 1024);
+		i = connect_to_server(server, 1024);
 		printf("%d", i);
 	}
 	
-	send_to_server("Please");
+	send_to_server("Please"); //The uppercase "P" does matter
 	
 	char password[24]; 
 	receive_from_server(password);
